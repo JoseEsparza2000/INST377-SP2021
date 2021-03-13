d21c4c611d8e692a77cc82a3d879e081e65e2166
Jose Esparza <73491225+JAEMANA@users.noreply.github.com>
2021-03-12 19:02:09 -0500

    update

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,16 +1,9 @@
-function mapInit() {
-  // follow the Leaflet Getting Started tutorial here
-  return map;
-}
-
-async function dataHandler(mapObjectFromFunction) {
-  // use your assignment 1 data handling code here
-  // and target mapObjectFromFunction to attach markers
-}
-
-async function windowActions() {
-  const map = mapInit();
-  await dataHandler(map);
-}
-
-window.onload = windowActions;
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+}).addTo(mymap);
